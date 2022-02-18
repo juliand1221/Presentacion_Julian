7e306946bd76b30e28b197a6edfd95039f9e0a1f
juliand1221 <judt1221@hotmail.com>
2022-02-18 00:30:45 -0500

    Arreglo de estilos

---
+++ header.js
@@ -0,0 +1,9 @@
+
+var menubtn = document.getElementsByClassName("menu-icon");
+var menu = document.getElementsByClassName("navigation ul");
+
+menubtn.addEventListener("click", addClase );
+
+function addClase(){
+    
+}
